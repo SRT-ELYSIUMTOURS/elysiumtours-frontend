58aa877e089e9765e3314e87f2e1d3a86b00bf51
GubliAS <gsitobu@gmail.com>
2025-10-24 12:10:23 +0000

    added color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,66 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: {
+            default: "#FEFEFE",
+            hover: "#F7F7F7",
+            active: "#FDFDFD",
+          },
+          normal: {
+            default: "#F7F7F7",
+            hover: "#DEDEDE",
+            active: "#C6C6C6",
+          },
+          dark: {
+            default: "#B9B9B9",
+            hover: "#949494",
+            active: "#6F6F6F",
+            darker: "#565656",
+          },
+        },
+        secondary: {
+          light: {
+            default: "#F2EAF9",
+            hover: "#EBDFF5",
+            active: "#D6BEEB",
+          },
+          normal: {
+            default: "#7B2CBF",
+            hover: "#6F28AC",
+            active: "#622399",
+          },
+          dark: {
+            default: "#5C218F",
+            hover: "#4A1A73",
+            active: "#371456",
+            darker: "#2B0F43",
+          },
+
+        },
+        tertiary: {
+          light: {
+            default: "#EAEAEA",
+            hover: "#E0E0E0",
+            active: "#BEBEBE",
+          },
+          normal: {
+            default: "#2D2D2D",
+            hover: "#292929",
+            active: "#242424",
+          },
+          dark: {
+            default: "#222222",
+            hover: "#1B1B1B",
+            active: "#141414",
+            darker: "#101010",
+          },
+
+        }
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
