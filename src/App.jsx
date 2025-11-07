e62f7723ed33e3dfd24fe1e08f89a481102f2398
unknown <eamokuandoh@gmail.com>
2025-11-07 15:13:02 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 import Button from './components/ui/button';
-
+import {Filter, FilterOption} from './components/ui/filter';
 
 function App() {
   return (
   <>
-  <div className="font-raleway bg-black">
+  <div className="font-raleway">
 
   <h1 className='text-green-600'>Hello World
 
   </h1>
   <Button >Contact Us</Button>
+  <Filter >
+    <FilterOption value="option1">Option 1</FilterOption>
+    <FilterOption value="option2">Option 2</FilterOption>
+    <FilterOption value="option3">Option 3</FilterOption>
+  </Filter>
   </div>
   </>
   );
